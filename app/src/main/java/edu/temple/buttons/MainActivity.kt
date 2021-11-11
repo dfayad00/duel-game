9c34a24cf8aea3c989070741f7b6c6b97ac7ecbb
Danny <dfayad00@gmail.com>
2021-11-11 01:23:12 -0500

    changes

--- app/src/main/java/edu/temple/buttons/MainActivity.kt
+++ app/src/main/java/edu/temple/buttons/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.temple.buttons
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -28,12 +29,16 @@ class MainActivity : AppCompatActivity() {
             //player 1 move
             narrator1.text = p1move
             player2.health = player1.doMove(player2, p2move, p1move)
-            player2Health.text = player2.health.toString()
+            //Log.e("log","Player 1 $p1move player 2")
 
             //player 2 move
             narrator2.text = p2move
             player1.health = player2.doMove(player1, p1move, p2move)
             player1Health.text = player1.health.toString()
+            //Log.e("log","Player 2 $p2move player 1\n")
+
+            player1Health.text = player1.health.toString()
+            player2Health.text = player2.health.toString()
         }
     }
 }
